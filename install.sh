8bf9a4960a7d7b6ac508394a01ace5b9f7247199
oklopfer <104327997+oklopfer@users.noreply.github.com>
2024-08-07 22:54:57 -0400

    fixup

--- install.sh
+++ install.sh
@@ -1,13 +1,44 @@
 #!/usr/bin/env bash
 
+pacinst=()
+if  [[ -f /usr/bin/pacstall ]]; then
+    if ! [[ -f /usr/share/oh-my-zsh/oh-my-zsh.sh ]]; then
+        pacinst+=(oh-my-zsh-git)
+    fi
+    if ! [[ -f /usr/bin/neofetch ]]; then
+        pacinst+=(neofetch)
+    fi
+    pacstall -PI "${pacinst[@]}"
+else
+    echo "Ensure oh-my-zsh and neofetch are installed"
+fi
+
+inst=()
+if [[ -f /usr/bin/apt ]]; then
+    if ! [[ -f /usr/bin/eza ]]; then
+        inst+=(eza)
+    fi
+    if ! [[ -f /usr/bin/bat ]]; then
+        inst+=(bat)
+    fi
+    sudo apt install "${inst[@]}" -y
+else
+    echo "eza and bat are recommended"
+fi
+
 git submodule update --init --recursive
 
 if [[ -f /usr/share/oh-my-zsh/oh-my-zsh.sh ]]; then
-	sudo cp -r oh-my-zsh/* /usr/share/oh-my-zsh/oh-my-zsh
+    sudo cp -r oh-my-zsh/* /usr/share/oh-my-zsh
 else
-	cp -r oh-my-zsh/* ~/.oh-my-zsh
+    cp -r oh-my-zsh/* ~/.oh-my-zsh
 fi
 
-for i in p10k.zsh zshrc config; do
-	cp -r "${i}" ~/."${i}"
+sudo cp -r config/neofetch ~/.config
+
+for i in p10k.zsh zshrc; do
+    cp -r "${i}" ~/."${i}"
 done
+
+neofetch
+"$(which zsh)"
